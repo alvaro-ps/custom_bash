9ac39dec6f34e13a150a64df3aaec4d3cdd04e18
apastors <a.pastor.sanchez@gmail.com>
2018-03-14 23:08:41 +0100

    Added prompt docs

--- prompt.sh
+++ prompt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://github.com/magicmonty/bash-git-prompt
+# Download the gitprompt.sh file from the Github repo and place it
+# wherever consistently with the path indicated in the source below
 GIT_PROMPT_ONLY_IN_REPO=0
 GIT_PROMPT_THEME=Solarized
 source ~/.bash-git-prompt/gitprompt.sh

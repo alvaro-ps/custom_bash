62b9ac02107e399e295ae60764b8130484e43dc3
apastors <a.pastor.sanchez@gmail.com>
2018-03-14 23:10:43 +0100

    Add docs to fasd.sh

--- fasd.sh
+++ fasd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://github.com/clvv/fasd. Check there to see how to install
+
 # initialize fasd
 eval "$(fasd --init auto posix-alias bash-ccomp bash-ccomp-install)"
 alias v="f -ie vim"

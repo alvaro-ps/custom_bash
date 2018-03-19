9d022db50b4c39bb9b49989ac438c6986d0462cc
apastors <a.pastor.sanchez@gmail.com>
2018-03-19 19:43:58 +0100

    remove config line for vim-ipython

--- main.sh
+++ main.sh
@@ -8,7 +8,6 @@ set -o vi
 #set page up and down to browse through history
 bind '"\e[5~":history-search-backward'
 bind '"\e[6~":history-search-forward'
-stty stop undef # to unmap ctrl-s (for vim-ipython)
 export TERM=xterm-256color #(256 colors in the terminal, for solarized)
 
 basepath=$(dirname ${BASH_SOURCE[0]})

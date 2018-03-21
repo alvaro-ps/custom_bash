25a3123db8ee73491a121ba28f55b1522bc7c1d7
apastors <a.pastor.sanchez@gmail.com>
2018-03-21 21:34:48 +0100

    added bash aliases

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-alias playgroundve="source ~/venvs/ve/bin/activate"
-alias cro2ve="source ~/venvs/cro/bin/activate"
-alias cro3ve="source ~/venvs/cro3/bin/activate"
-alias py3="source ~/venvs/py3/bin/activate"
+alias ll="ls -alh"
+alias la="ls -a"
+alias l="ls"

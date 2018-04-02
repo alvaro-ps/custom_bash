48b582a93db905c73957c34ce125dc0f326435ac
apastors <a.pastor.sanchez@gmail.com>
2018-04-02 16:48:55 +0200

    add status config

--- apps/i3/init.sh
+++ apps/i3/init.sh
@@ -1,2 +1,3 @@
 wd=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-ln -f $wd"/config" ~/.config/i3/config
+ln -f $wd"/main_config" ~/.config/i3/config
+ln -f $wd"/status_config" ~/.config/i3status/config

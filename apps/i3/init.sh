5610c229e28b2873eae4ad4abdfac78e1af34a54
apastors <alvaro@predicube.com>
2018-05-09 13:59:14 +0200

    added comment

--- apps/i3/init.sh
+++ apps/i3/init.sh
@@ -2,5 +2,6 @@ wd=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 ln -f $wd"/main_config" ~/.config/i3/config
 ln -f $wd"/status_config" ~/.config/i3status/config
 
+# SSH agent
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK

f9cac51903b10179212bd1711db46a7c15098b37
apastors <alvaro@predicube.com>
2018-11-11 20:16:28 +0100

    i3blocks

--- apps/i3/init.sh
+++ apps/i3/init.sh
@@ -1,6 +1,7 @@
 wd=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 ln -f $wd"/main_config" ~/.config/i3/config
 ln -f $wd"/status_config" ~/.config/i3status/config
+ln -f $wd"/i3blocks.conf" ~/.config/i3blocks/i3blocks.conf
 
 # SSH agent
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)

814759bcea43c6a71b8efcbfbb60b0261fc99ab3
apastors <alvaro@predicube.com>
2018-11-12 09:28:41 +0100

    remove i3status config

--- apps/i3/init.sh
+++ apps/i3/init.sh
@@ -1,6 +1,5 @@
 wd=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 ln -f $wd"/main_config" ~/.config/i3/config
-ln -f $wd"/status_config" ~/.config/i3status/config
 ln -f $wd"/i3blocks.conf" ~/.config/i3blocks/i3blocks.conf
 
 # SSH agent

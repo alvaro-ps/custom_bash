4adfdaf4ef16b790286deade30e060a75e582d94
apastors <alvaro@predicube.com>
2018-11-12 23:39:18 +0100

    polybar

---
+++ apps/polybar/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://github.com/jaagr/polybar
+
+# link config to ~/.config/polybar/
+wd=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+ln -f $wd"/config" ~/.config/polybar/config
+
+source displaytitle
---
+++ apps/polybar/install.sh
@@ -0,0 +1,1 @@
+yaourt -S polybar-git

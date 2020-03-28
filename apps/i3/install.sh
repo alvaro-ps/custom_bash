f530078c557dc8b6bac8e0ceab571ebaeeeb4581
apastors <a.pastor.sanchez@gmail.com>
2020-03-28 17:17:57 +0000

    add dmenu to i3 install

--- apps/i3/install.sh
+++ apps/i3/install.sh
@@ -1,5 +1,5 @@
 # install i3
-sudo pacman -S i3
+sudo pacman -S i3 dmenu
 # install scripts for i3blocks (temperature, calendar, ...)
 rm -rf ~/.config/i3blocks/
 git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks/scripts/

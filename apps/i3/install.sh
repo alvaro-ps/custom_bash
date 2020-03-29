3465a91998fe34e98f1315cbf246415d7ca74b18
apastors <a.pastor.sanchez@gmail.com>
2020-03-29 10:04:03 +0100

    replace dmenu with synapse

--- apps/i3/install.sh
+++ apps/i3/install.sh
@@ -1,8 +1,8 @@
 # install i3 and extra stuff
-# dmenu is the launcher
+# synapse is the launcher
 # nitrogen to set backgrounds
 # playerctl to play/pause with keyboard
-sudo pacman -S i3 dmenu nitrogen playerctl
+sudo pacman -S i3 synapse nitrogen playerctl
 # install scripts for i3blocks (temperature, calendar, ...)
 rm -rf ~/.config/i3blocks/
 git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks/scripts/

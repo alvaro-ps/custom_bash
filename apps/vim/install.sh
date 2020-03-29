09ebcb71c657e36c379780d917358b3f77f9a5b0
apastors <a.pastor.sanchez@gmail.com>
2020-03-29 17:57:50 +0100

    install gvim by default

--- apps/vim/install.sh
+++ apps/vim/install.sh
@@ -1,3 +1,3 @@
 # GUI vim, which has by default clipboard among other nice things
-sudo pacman -S gvim
+sudo pacman -S vim gvim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

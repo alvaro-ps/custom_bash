59396ca4bde6e78b6da316c40490a42115214129
apastors <a.pastor.sanchez@gmail.com>
2020-03-29 09:48:58 +0100

    install playerctl and set zoom fill for backgrounds

--- apps/i3/install.sh
+++ apps/i3/install.sh
@@ -1,6 +1,8 @@
 # install i3 and extra stuff
-# dmenu is the launcher, nitrogen to set backgrounds
-sudo pacman -S i3 dmenu nitrogen
+# dmenu is the launcher
+# nitrogen to set backgrounds
+# playerctl to play/pause with keyboard
+sudo pacman -S i3 dmenu nitrogen playerctl
 # install scripts for i3blocks (temperature, calendar, ...)
 rm -rf ~/.config/i3blocks/
 git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks/scripts/

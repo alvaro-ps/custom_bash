01d3606c4e3ac6861abc7fb324274ec69c452e74
apastors <a.pastor.sanchez@gmail.com>
2020-04-13 18:48:06 +0100

    add backgrounds and select 2 randomly

--- apps/i3/init.sh
+++ apps/i3/init.sh
@@ -4,6 +4,8 @@ ln -f $wd"/main_config" ~/.config/i3/config
 mkdir -p ~/.config/i3blocks
 ln -f $wd"/i3blocks.conf" ~/.config/i3blocks/i3blocks.conf
 
+ln -s $wd"/backgrounds/" ~/Pictures/
+
 # SSH agent
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK

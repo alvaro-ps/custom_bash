96b0a882677a856fdec0e038a2bd43c22b94dd36
apastors <a.pastor.sanchez@gmail.com>
2020-05-07 09:54:15 +0100

    move to vimplug and coc for autocompletion

--- apps/vim/install.sh
+++ apps/vim/install.sh
@@ -1,3 +1,4 @@
 # GUI vim, which has by default clipboard among other nice things
 sudo pacman -S vim gvim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+pip install --user -U python-language-server jedi pylint

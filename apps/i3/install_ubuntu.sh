194837808d2b15e7620dec1e90ea544a2588eb1c
apastors <a.pastor.sanchez@gmail.com>
2020-07-28 13:01:12 +0100

    install i3 gaps and i3status

--- apps/i3/install_ubuntu.sh
+++ apps/i3/install_ubuntu.sh
@@ -1,8 +1,11 @@
+# to install i3 gaps
+sudo add-apt-repository -y ppa:kgilmer/speed-ricer
+sudo apt-get update
 # install i3 and extra stuff
 # synapse is the launcher
 # nitrogen to set backgrounds
 # playerctl to play/pause with keyboard
-sudo apt install i3 synapse nitrogen playerctl
+sudo apt install -y i3-gaps i3status i3blocks synapse nitrogen playerctl
 # install scripts for i3blocks (temperature, calendar, ...)
 rm -rf ~/.config/i3blocks/
 git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks/scripts/

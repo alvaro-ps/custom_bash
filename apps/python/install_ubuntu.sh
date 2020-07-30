e2b9a4541e15cef3696134d5d2294c4c0c487109
apastors <a.pastor.sanchez@gmail.com>
2020-07-30 08:49:23 +0100

    pip -> pip3

--- apps/python/install_ubuntu.sh
+++ apps/python/install_ubuntu.sh
@@ -1,6 +1,7 @@
 sudo apt install python-is-python3
 # install pip
 sudo apt install python3-pip
+sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 
 # install virtualenv
 sudo pip install virtualenv

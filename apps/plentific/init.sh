4536d6a6d5300333743cfb2caf2cc957b0432d1b
apastors <alvaro.pastor-sanchez@plentific.com>
2020-08-04 16:53:20 +0100

    remove unused stuff

--- apps/plentific/init.sh
+++ apps/plentific/init.sh
@@ -6,17 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 # AVN - automatically select node version based on a .node-version file
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
 
-alias p='docker-compose -f ~/plentific/infrastructure/docker-compose/docker-compose.yaml'
-
-pullall () {
-    for repo_dir in ~/plentific/*; do 
-        pushd $repo_dir >> /dev/null
-        branch=$(git branch | grep \* | cut -d " " -f2)
-        echo "Pulling latest version of $branch on $repo_dir"
-        pull
-        popd >> /dev/null
-    done
-}
-
 export PLENTIFIC_WORKSPACE_ROOT=$HOME/plentific/workspaces/
 source /home/alvaro/plentific/docker-ide-workspace/cli_bashcompletion.sh
+
+export PYTHONPATH=$PYTHONPATH:/home/alvaro/plentific/plentific-common/

cd18110a38e4a0d2f501c8d5b758c53873766e7b
apastors <alvaro.pastor-sanchez@plentific.com>
2020-08-07 08:19:22 +0100

    pre-commit configfile

--- apps/python/init.sh
+++ apps/python/init.sh
@@ -1,3 +1,6 @@
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+# to install the pre-commit in a repo, just run:
+# pre-commit install -c /path/to/pre-commit-config.yaml

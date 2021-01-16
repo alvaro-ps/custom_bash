6252cee03f92333ecd109e90684123928ca7ad12
apastors <alvaro.pastor-sanchez@plentific.com>
2021-01-16 17:11:39 +0000

    pyenv disable prompt

--- apps/python/init.sh
+++ apps/python/init.sh
@@ -1,6 +1,6 @@
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 # to install the pre-commit in a repo, just run:
 # pre-commit install -c /path/to/pre-commit-config.yaml

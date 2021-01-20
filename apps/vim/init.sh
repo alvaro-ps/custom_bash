6b3274e0343559f192fd7cc39458862d21afa6e1
apastors <alvaro.pastor-sanchez@plentific.com>
2021-01-20 16:29:57 +0000

    add vck to open files in vim given a regex

--- apps/vim/init.sh
+++ apps/vim/init.sh
@@ -1,3 +1,9 @@
 basepath=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 # hard link to the custom vimrc file
 ln -f $basepath"/vimrc" ~/.vimrc
+
+vck () {
+    # open files that match the given regex
+    REGEX=$1
+    ack -l $1 | xargs -o vim +/$1
+}

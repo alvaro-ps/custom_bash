6b508995ed71425f8018b6e18c8ad11dc5b1ab4f
apastors <a.pastor.sanchez@gmail.com>
2022-01-03 13:22:21 +0000

    remove plentific stuff

--- apps/plentific/init.sh
+++ apps/plentific/init.sh
@@ -1,19 +1,4 @@
-# NVM - node version manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# AVN - automatically select node version based on a .node-version file
-[[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh" # load avn
-
 export PLENTIFIC_WORKSPACE_ROOT=$HOME/plentific/workspaces/
-source /home/alvaro/plentific/docker-ide-workspace/cli_bashcompletion.sh
-
-export PYTHONPATH=$PYTHONPATH:/home/alvaro/plentific/plentific-common/
-export PYTHONPATH=$PYTHONPATH:/home/alvaro/plentific/plentific-upload/
-export PYTHONPATH=$PYTHONPATH:/home/alvaro/plentific/plentific-locations/
-export PYTHONPATH=$PYTHONPATH:/home/alvaro/plentific/plentifone/
-export PYTHONPATH=$PYTHONPATH:/home/alvaro/plentific/emails-python-client/
 
 #kubectx and kubens
 export PATH=~/.kubectx:$PATH

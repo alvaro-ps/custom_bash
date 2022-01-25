9a857695a1f65add47d4b475a2c945b78129305e
alvaro <alvaro@hummingbirdtech.com>
2022-01-25 11:09:40 +0000

    install packages before pyenv

--- apps/python/install.sh
+++ apps/python/install.sh
@@ -4,5 +4,7 @@ sudo pacman -S python-pip
 # install virtualenv
 pip install virtualenv
 
+sudo pacman -S --needed base-devel openssl zlib xz
+
 # install pyenv
 curl https://pyenv.run | bash
--- apps/python/install_ubuntu.sh
+++ apps/python/install_ubuntu.sh
@@ -7,5 +7,11 @@ sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 sudo pip install virtualenv
 
 # install pyenv
-sudo apt install curl
+
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+sudo apt-get update
+sudo apt-get install \
+    make build-essential libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 curl https://pyenv.run | bash

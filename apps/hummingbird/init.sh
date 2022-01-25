61f806d3ef1accd5667fb226d864276f654a9545
alvaro <alvaro@hummingbirdtech.com>
2022-01-25 10:55:05 +0000

    add vpn_toggle and update readme

--- apps/hummingbird/init.sh
+++ apps/hummingbird/init.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
 VPN_SERVICE="openvpn@hummingbird.service"
+
 vpn_on() {
     sudo systemctl restart $VPN_SERVICE
 }
 
 vpn_off() {
     sudo systemctl stop $VPN_SERVICE
+}
+
+vpn_toggle() {
+    vpn_status=$(systemctl is-active $VPN_SERVICE)
+    if [[ "$vpn_status" == "active" ]]; then
+        vpn_off
+    else
+        vpn_on
+    fi
 }

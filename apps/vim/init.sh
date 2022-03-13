62fc7c22cb055fe80454ffccf05e52623af83e9d
alvaro <alvaro@hummingbirdtech.com>
2022-03-13 16:51:17 +0000

    use strings in the "regex", in case there are spaces

--- apps/vim/init.sh
+++ apps/vim/init.sh
@@ -6,16 +6,16 @@ ln -f $basepath"/coc-settings.json" ~/.vim/coc-settings.json
 
 vck () {
     # open files that match the given regex
-    REGEX=$1
+    REGEX="$1"
     DIRECTORY=$2
 
-    FILES_MATCHING_REGEX=$(ack -l $REGEX $DIRECTORY)
+    FILES_MATCHING_REGEX=$(ack -l "$REGEX" $DIRECTORY)
     if [ -z "$FILES_MATCHING_REGEX" ]; then
-        echo "No files matching '$REGEX'"
+        echo "No files matching '"$REGEX"'"
         return
     fi
     # remove trailing slashes from the regex that will be passed to VIM, as it does not
     # need them
-    VIM_REGEX=$(echo $REGEX | tr -d '\\')
-    echo $FILES_MATCHING_REGEX | xargs -o vim +/$VIM_REGEX
+    VIM_REGEX=$(echo "$REGEX" | tr -d '\\')
+    echo "$FILES_MATCHING_REGEX" | xargs -o vim +/"$VIM_REGEX"
 }

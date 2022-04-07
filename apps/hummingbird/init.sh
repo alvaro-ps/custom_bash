d4a8c9f6be40cff1eca2487bc9b994adeadfe563
alvaro <alvaro@hummingbirdtech.com>
2022-04-07 17:43:52 +0100

    move k8s stuff to hummingbird and delete remaining plentific stuff

--- apps/hummingbird/init.sh
+++ apps/hummingbird/init.sh
@@ -21,6 +21,25 @@ vpn_toggle() {
 
 export VAULT_ADDR=https://vault.infra.hummingbird-tech.io:8200
 
+## Kubernetes (needs installation!!!)
+alias k="kubectl"
+alias kls="kubectl get pods"
+
+go_pod () {
+    # bash shell in the web pod given by service name
+    SERVICE_NAME=$1
+    MATCH="$SERVICE_NAME.*web"
+    POD_NAME=$(kubectl get pods -o name | grep "$MATCH")
+    if [ -z "$POD_NAME" ]; then
+        echo -e "No pod found that matches $MATCH"
+        return 1
+    fi
+    kubectl exec --stdin --tty "$POD_NAME" -- /bin/bash
+}
 
 # Krew
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# kubectl tab-completion (see bash-completion in main ./install_ubuntu.sh)
+source <(kubectl completion bash)
+complete -F __start_kubectl k
--- apps/plentific/install_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-# kubectx
-rm -rf ~/.kubectx
-git clone https://github.com/ahmetb/kubectx.git ~/.kubectx
-COMPDIR=$(pkg-config --variable=completionsdir bash-completion)
-sudo ln -sf ~/.kubectx/completion/kubens.bash $COMPDIR/kubens
-sudo ln -sf ~/.kubectx/completion/kubectx.bash $COMPDIR/kubectx

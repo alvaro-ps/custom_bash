a9048cb4dc622cffbcd1b4e5c4db7b8196f6da0a
alvaro <alvaro@hummingbirdtech.com>
2022-08-04 13:49:14 +0100

    try to get rid of

--- apps/i3/init.sh
+++ apps/i3/init.sh
@@ -12,3 +12,4 @@ fi
 # SSH agent
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+dbus-update-activation-environment --systemd DISPLAY
